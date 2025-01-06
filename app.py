0f6da17f5c8ac34cdb79a4ca724e205420d625f0
jebko0429 <124783605+jebko0429@users.noreply.github.com>
2025-01-06 22:34:57 +0800

    .

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('chat.html')
+    return render_template('index.html')
 
 @socketio.on('send_message')
 def handle_message(data):
